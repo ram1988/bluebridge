66f2754f794d262b52ef4219a88c2e7b12cc652b
SIQ Automation <colichia@us.ibm.com>
2015-11-02 17:09:18 +0800

    commit

--- app/src/main/java/com/ibm/bluebridge/EventParentDetailActivity.java
+++ app/src/main/java/com/ibm/bluebridge/EventParentDetailActivity.java
@@ -2,14 +2,10 @@ package com.ibm.bluebridge;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.widget.TextView;
 
-import com.ibm.bluebridge.valueobject.Event;
 import com.ibm.bluebridge.valueobject.Parent;
 
 public class EventParentDetailActivity extends AppCompatActivity {
