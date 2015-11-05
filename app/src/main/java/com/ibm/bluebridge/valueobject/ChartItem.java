43851f8c36a8680fa38987b0f51c459614b2f08e
Huang Qi <huangq@sg.ibm.com>
2015-11-05 17:58:15 +0800

    add in a testing page for PMAndroidChart, generated line chart. Also add in take snapshot and send the image as attachment using system email. Add a list view page for statistics

--- app/src/main/java/com/ibm/bluebridge/valueobject/ChartItem.java
+++ app/src/main/java/com/ibm/bluebridge/valueobject/ChartItem.java
@@ -2,6 +2,18 @@ package com.ibm.bluebridge.valueobject;
 
 /**
  * Created by huangq on 5/11/2015.
+ *
+ * This the chart item object for Statistics page.
+ * Cause statistics page is list view, have to define an adapter.
  */
-public class ChartItem {
+public class ChartItem extends BlueBridgeVO{
+    private String category;
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
