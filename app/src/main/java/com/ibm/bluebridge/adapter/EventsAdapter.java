afa14129d5d7b786601dedf972b98bfcd28b5318
Serena XH Li <serena_li@sg.ibm.com>
2015-11-05 11:46:43 +0800

    renable duration, registered, attended

--- app/src/main/java/com/ibm/bluebridge/adapter/EventsAdapter.java
+++ app/src/main/java/com/ibm/bluebridge/adapter/EventsAdapter.java
@@ -388,10 +388,10 @@ public class EventsAdapter {
                         event.setMaxVolunteers(item.getInt("max_volunteers"));
                         event.setVacancy(item.getInt("event_vacancy"));
                         event.setCategory(item.getString("category"));
-                       /* event.setDuration(item.getString("duration_in_hour"));
+                        event.setDuration(item.getString("duration_in_hour"));
                         event.setRegistered(item.getBoolean("registered"));
                         event.setAttended(item.getBoolean("attended"));
-*/
+
                         eventsList.add(event);
 
                         //System.out.println("jsonobj--->" + item.get("name"));
@@ -447,10 +447,10 @@ public class EventsAdapter {
                         event.setMaxVolunteers(item.getInt("max_volunteers"));
                         event.setVacancy(-100);
                         event.setCategory(item.getString("category"));
-                        /*event.setDuration(item.getString("duration_in_hour"));
+                        event.setDuration(item.getString("duration_in_hour"));
                         event.setRegistered(item.getBoolean("registered"));
                         event.setAttended(item.getBoolean("attended"));
-*/
+
                         eventsList.add(event);
 
                     }
@@ -505,10 +505,10 @@ public class EventsAdapter {
                         event.setMaxVolunteers(item.getInt("max_volunteers"));
                         event.setVacancy(-100);
                         event.setCategory(item.getString("category"));
-                        /*event.setDuration(item.getString("duration_in_hour"));
+                        event.setDuration(item.getString("duration_in_hour"));
                         event.setRegistered(item.getBoolean("registered"));
                         event.setAttended(item.getBoolean("attended"));
-*/
+
                         eventsList.add(event);
                     }
                 } else {
