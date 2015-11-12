3be09c17dd025c99128fff58e7326a8d527ead8a
Serena XH Li <serena_li@sg.ibm.com>
2015-11-12 11:42:53 +0800

    fix parent view bug

--- app/src/main/java/com/ibm/bluebridge/EventLoginActivity.java
+++ app/src/main/java/com/ibm/bluebridge/EventLoginActivity.java
@@ -267,12 +267,12 @@ public class EventLoginActivity extends ActionBarActivity implements LoaderCallb
 
             if (role!=null) {
                 Intent intent = null;
-                intent = new Intent(ctxt, EventAdminHomeTabActivity.class);
-               /* if(role.equals("admin")) {
+                //intent = new Intent(ctxt, EventAdminHomeTabActivity.class);
+                if(role.equals("admin")) {
                     intent = new Intent(ctxt, EventAdminHomeTabActivity.class);
                 } else {
                     intent = new Intent(ctxt, EventParentViewActivity.class);
-                }*/
+                }
                 intent.putExtra("user_id",nric);
                 startActivity(intent);
                 finish();
