d07d85149848f1626488a01c4a7c7a38f9daf558
Serena XH Li <serena_li@sg.ibm.com>
2015-11-17 15:54:33 +0800

    add aboutme for parent

--- app/src/main/java/com/ibm/bluebridge/EventParentHomeSpinnerActivity.java
+++ app/src/main/java/com/ibm/bluebridge/EventParentHomeSpinnerActivity.java
@@ -1,6 +1,10 @@
 package com.ibm.bluebridge;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
@@ -20,22 +24,30 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.ScrollView;
 import android.widget.Spinner;
 import android.content.Context;
 import android.support.v7.widget.ThemedSpinnerAdapter;
 import android.content.res.Resources.Theme;
 
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ibm.bluebridge.adapter.EventsAdapter;
 import com.ibm.bluebridge.eventcalendar.CalendarManager;
 import com.ibm.bluebridge.adapter.StatisticsAdapter;
 import com.ibm.bluebridge.eventcalendar.EventCalendarView;
+import com.ibm.bluebridge.util.RESTApi;
 import com.ibm.bluebridge.util.SessionManager;
 import com.ibm.bluebridge.valueobject.ChartItem;
+import com.ibm.bluebridge.valueobject.Children;
 import com.ibm.bluebridge.valueobject.Event;
+import com.ibm.bluebridge.valueobject.Parent;
 
+import java.io.InputStream;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -73,7 +85,7 @@ public class EventParentHomeSpinnerActivity extends EventMasterActivity {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
-                if(position == 5) {  //Last item is logout
+                if (position == 5) {  //Last item is logout
                     session.logout();
                     return;
                 }
@@ -156,6 +168,11 @@ public class EventParentHomeSpinnerActivity extends EventMasterActivity {
      * A placeholder fragment containing a simple view.
      */
     public static class PlaceholderFragment extends Fragment {
+        private ProgressDialog pDialog;
+        private Bitmap bitmap;
+        private ImageView photo;
+        private RESTApi REST_API;
+
         /**
          * The fragment argument representing the section number for this
          * fragment.
@@ -188,10 +205,12 @@ public class EventParentHomeSpinnerActivity extends EventMasterActivity {
             //final EventsAdapter eventsAdapter = new EventsAdapter(selfCtxt);
             final ListView listView = (ListView) rootView.findViewById(R.id.listview);
             TextView noEventsMsg = (TextView)rootView.findViewById(R.id.no_events_message);
+            ScrollView parentDetailView = (ScrollView)rootView.findViewById(R.id.parent_details);
             ArrayAdapter<Event> adapter = null;
 
             //For all events
             if(tabNumber == 1 ) {
+                parentDetailView.setVisibility(View.INVISIBLE);
                 final List<Event> eventList = eventsAdapter.getAllEventsList(parent_id);
 
                 if(eventList.isEmpty()){
@@ -223,6 +242,7 @@ public class EventParentHomeSpinnerActivity extends EventMasterActivity {
             }
             //For joined events
             else if(tabNumber == 2) {
+                parentDetailView.setVisibility(View.INVISIBLE);
                 final List<Event> eventList = eventsAdapter.getAllJoinedEventsList(parent_id);
 
                 CalendarManager calendarManager = new CalendarManager(selfCtxt);
@@ -260,6 +280,7 @@ public class EventParentHomeSpinnerActivity extends EventMasterActivity {
             }
             //For attended events
             else if(tabNumber == 3) {
+                parentDetailView.setVisibility(View.INVISIBLE);
                 final List<Event> eventList = eventsAdapter.getAllAttendedEventsList(parent_id);
 
                 CalendarManager calendarManager = new CalendarManager(selfCtxt);
@@ -294,6 +315,7 @@ public class EventParentHomeSpinnerActivity extends EventMasterActivity {
                     });
                 }
             }else if (tabNumber == 4){
+                parentDetailView.setVisibility(View.INVISIBLE);
                 //For Statistics
                 List<ChartItem> charts = new ArrayList<ChartItem>();
 
@@ -322,10 +344,70 @@ public class EventParentHomeSpinnerActivity extends EventMasterActivity {
                 viewCalendarButton.setVisibility(View.INVISIBLE);
 
             }else if (tabNumber == 5){
+                viewCalendarButton.setVisibility(View.INVISIBLE);
                 //For About Me
+                Parent parent = eventsAdapter.getParentDetail(parent_id);
+                //this.setTitle(parent.getFirstname()+" "+parent.getLastname());
+
+                TextView nameView = (TextView) parentDetailView.findViewById(R.id.name_text);
+                TextView nricView = (TextView) parentDetailView.findViewById(R.id.nric_text);
+                TextView genderView = (TextView) parentDetailView.findViewById(R.id.gender_text);
+                TextView contactView = (TextView) parentDetailView.findViewById(R.id.contact_text);
+                TextView emailView = (TextView) parentDetailView.findViewById(R.id.email_text);
+                TextView jobView = (TextView) parentDetailView.findViewById(R.id.job_text);
+                TextView addressView = (TextView) parentDetailView.findViewById(R.id.address_text);
+                ListView childrenView = (ListView) parentDetailView.findViewById(R.id.childrenlist);
+
+                this.REST_API = new RESTApi();
+                photo = (ImageView) parentDetailView.findViewById(R.id.photo);
+                String imageURL = REST_API.getBaseRestURL() + "/view_user_image?user_id=" + parent_id;
+                new LoadImage().execute(imageURL);
+
+                nameView.setText("Name:         " + parent.getFirstname() + " " + parent.getLastname());
+                nricView.setText("NRIC:           " + parent.getId());
+                genderView.setText( "Gender:        " + parent.getGender());
+                contactView.setText("Contact:       " + parent.getContact());
+                emailView.setText("Email:          " + parent.getEmail());
+                jobView.setText(    "Job:              " + parent.getJob());
+                addressView.setText("Address:      " + parent.getAddress());
+
+                ArrayAdapter<Children> mAdapter = getChildrenListItemAdapter(selfCtxt, parent.getChildren());
+                childrenView.setAdapter(mAdapter);
             }
 
             return rootView;
+        }
+
+        private class LoadImage extends AsyncTask<String, String, Bitmap> {
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                pDialog = new ProgressDialog(selfCtxt);
+                pDialog.setMessage("Loading Image ....");
+                pDialog.show();
+
+            }
+            protected Bitmap doInBackground(String... args) {
+                try {
+                    bitmap = BitmapFactory.decodeStream((InputStream) new URL(args[0]).getContent());
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return bitmap;
+            }
+
+            protected void onPostExecute(Bitmap image) {
+
+                if(image != null){
+                    photo.setImageBitmap(image);
+                    pDialog.dismiss();
+                }else{
+                    pDialog.dismiss();
+                    Toast.makeText(selfCtxt, "Image Does Not exist or Network Error", Toast.LENGTH_SHORT).show();
+
+                }
+            }
         }
     }
 
